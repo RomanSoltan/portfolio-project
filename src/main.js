72673712e1cc22a5727662c9d09d2a24bd848e1f
Roman Soltan <romabandit88@gmail.com>
2024-12-31 22:19:24 +0200

    import html, css, js-files

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/header';
+import './js/about-section';
+import './js/projects-section';
+import './js/faq-section';
+import './js/covers-section';
+import './js/reviews-section';
+import './js/footer';
